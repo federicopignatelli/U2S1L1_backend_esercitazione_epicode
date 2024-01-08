cabfb90f6ee8fe9bcfb60f416d5877ab0601f695
federicopignatelli <federicopignatelli@icloud.com>
2024-01-08 16:51:07 +0100

    Initial commit

---
+++ src/main/java/federicopignatelli/U2S1L1_backend_esercitazione_epicode/U2S1L1BackendEsercitazioneEpicodeApplication.java
@@ -0,0 +1,13 @@
+package federicopignatelli.U2S1L1_backend_esercitazione_epicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2S1L1BackendEsercitazioneEpicodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2S1L1BackendEsercitazioneEpicodeApplication.class, args);
+	}
+
+}
